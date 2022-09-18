fbb24e77336a893da87ad0aa85b0d97f3972bbcd
alandsilva01 <111643865+alandsilva01@users.noreply.github.com>
2022-09-18 09:12:22 -0300

    aula013

---
+++ exercícios/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 4) // essa estrutura de repetição ira repetir ate a var 'c' for maior ou igual ao numero especificado
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 500){
+    console.log('Tudo bem?')
+    c++
+}
+*/ //aqui tambem 
